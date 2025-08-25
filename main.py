5a0dcaeb90e74d573d0656e3ffaa664946ad1310
010190 <olczakmichal4@gmail.com>
2025-08-25 14:08:32 +0200

    level content add

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ app = Tk()
 app.geometry("500x500")
 app.title("Typing Speedtest")
 
-text = ("A fitness studio was struggling to attract new members. "
+normal_text = ("A fitness studio was struggling to attract new members. "
         "The owner, Maria, decided to launch a 30-day fitness challenge with a $500 cash prize for the most improved participant. "
         "The challenge went viral on social media, generating 500 new leads within the first week. "
         "By the end of the month, the studio had gained 150 new members, "
-        "a 20% increase in revenue, and a revitalized community of fitness enthusiasts.").replace(".","").replace(",","").lower().strip().split()
+        "a 20% increase in revenue, and a revitalized community of fitness enthusiasts.")
+easy_text = ("The world of typing and office technology is constantly evolving. "
+             "Invest in continuing education to stay up-to-date with the latest trends and technologies. "
+             "Consider pursuing certifications in specialized areas to enhance your skillset and marketability.")
 
+
+hard_text = ("Teamwork is not only about achieving goals; it's also about the joy of collaboration, "
+             "the satisfaction of working together towards a common purpose, "
+             "and the sense of camaraderie that comes from being part of something bigger than oneself. "
+             "When team members feel connected to each other and to their shared mission, "
+             "they are more engaged, motivated, and productive. "
+             "They are also more likely to experience a sense of fulfillment and satisfaction in their work. "
+             "The joy of teamwork is a powerful force that can transform workplaces, communities, and even the world.")
 
 count = 0
 row  = 0
@@ -37,117 +48,123 @@ welcome_label.place(x=8,y=20)
 choose_label = Label(app, text="Choose a level:", font=("Arial", 20, "bold"),background="#FE8D6F", foreground="white")
 choose_label.place(x=160,y=75)
 
-easy_button = Button(app, text="Easy")
+easy_button = Button(app, text="Easy", command=lambda:new_dialog(easy_text, "easy"))
 easy_button.place(x=225,y=120)
 
-normal_button = Button(app, text="Normal")
+normal_button = Button(app, text="Normal", command=lambda:new_dialog(normal_text, "normal"))
 normal_button.place(x=225,y=150)
 
 
-hard_button = Button(app, text="Hard")
+hard_button = Button(app, text="Hard", command=lambda:new_dialog(hard_text, "hard"))
 hard_button.place(x=225,y=180)
 
+def new_dialog(content,level):
+        global row, count
+        window = Toplevel(app)
+        window.geometry("500x500")
+        text = content.replace(".","").replace(",","").lower().strip().split()
+        level_label = Label(window, text=f"Typing speed test: level {level}", font=("Arial", 25, 'bold'),
+                              background="#FE8D6F", foreground="white")
+        level_label.place(x=9, y=20)
+        frame = Frame(window,width=450, height=350, borderwidth=2, relief="solid")
+        frame.place(x=100, y=75)
+        for text_item in text:
+            count +=1
+            label = Label(frame, text=text_item, font=("Arial", 8))
 
-# frame = Frame(app,width=450, height=350, borderwidth=2, relief="solid")
-# frame.place(x=100, y=75)
-# for text_item in text:
-#     count +=1
-#     label = Label(frame, text=text_item, font=("Arial", 8))
-#
-#     if count != 5:
-#         label.grid(column=count, row=row)
-#     else:
-#         row += 1
-#         count = 0
-#         label.grid(column=count, row=row)
-#     label_list.append(label)
-#
-# user_var = StringVar()
-#
-# entry = Entry(app, width=45, textvariable=user_var, font=("Arial", 8))
-# entry.place(x=100, y=350)
-#
-#
-# text_label_list = [label["text"] for label in label_list]
-#
-#
-# def speed_test(event):
-#     global correct_count, wrong_count, character_count
-#     user_input = user_var.get().strip()
-#     if user_input in text:
-#         for item in label_list:
-#             if item["background"] == "" and user_input == item["text"]:
-#                 item.configure(background="green")
-#                 break
-#         correct_words.append(user_input)
-#         correct_count += 1
-#         character_count += len(label["text"])
-#     else:
-#         for item in label_list[::-1]:
-#             if item["background"] != "":
-#                 id = label_list.index(item)
-#                 label_list[id + 1].configure(background="red")
-#                 break
-#         if label_list[0]["background"] == "":
-#             label_list[0].configure(background="red")
-#         wrong_count += 1
-#     try:
-#         entry.delete(0, END)
-#     except TclError:
-#         print(correct_words, wrong_count)
-#
-# def check():
-#     global time_value
-#     # multiplier = 60 / time_value
-#     wpm_value = correct_count * 2
-#     cpm_value = character_count * 2
-#     wpm_label.configure(text=f"WPM:{wpm_value}")
-#     cpm_label.configure(text=f"CPM:{cpm_value}")
-#
-# time_label = Label(app, text="00:00", font=("Arial", 15))
-# time_label.place(x=400,y=100)
-#
-# def counter():
-#     global  time_value,counter_id
-#     entry = Entry(app, width=45, textvariable=user_var, font=("Arial", 8))
-#     entry.place(x=100, y=350)
-#     if time_value > 0:
-#         time_label.configure(text=f"00:{time_value}")
-#         time_value -= 1
-#         counter_id = app.after(1000, counter)
-#
-#     if time_value < 10 and time_value != 0:
-#         time_value -= 1
-#         time_label.configure(text=f"00:0{time_value}")
-#     if time_value == 0:
-#         time_label.configure(text=f"00:00")
-#
-#         entry.destroy()
-#         check()
-#     app.bind('<space>', speed_test)
-#
-#
-# def reset():
-#     global time_value
-#     time_value = 30
-#     app.after_cancel(counter_id)
-#     for label in label_list:
-#         label.configure(background="")
-#     wpm_label.configure(text="WPM:", font=("Arial", 15))
-#     time_label.configure(text="00:00")
-#     cpm_label.configure(text="CPM:", font=("Arial", 15))
-#
-# start_button = Button(app, text="Start", command=counter)
-# start_button.place(x=220,y=380)
-#
-# reset_button = Button(app, text="reset", command=reset)
-# reset_button.place(x=220,y=400)
-#
-# wpm_label = Label(app, text="WPM:", font=("Arial", 12))
-# cpm_label = Label(app, text="CPM:", font=("Arial", 12))
-#
-#
-# wpm_label.place(x=400,y=125)
-# cpm_label.place(x=400,y=150)
+            if count != 5:
+                label.grid(column=count, row=row)
+            else:
+                row += 1
+                count = 0
+                label.grid(column=count, row=row)
+            label_list.append(label)
+
+        user_var = StringVar()
+
+        entry = Entry(window, width=45, textvariable=user_var, font=("Arial", 8))
+        entry.place(x=100, y=350)
+
+
+        text_label_list = [label["text"] for label in label_list]
+
+
+        def speed_test(event):
+            global correct_count, wrong_count, character_count
+            user_input = user_var.get().strip()
+            if user_input in text:
+                for item in label_list:
+                    if item["background"] == "" and user_input == item["text"]:
+                        item.configure(background="green")
+                        break
+                correct_words.append(user_input)
+                correct_count += 1
+                character_count += len(label["text"])
+            else:
+                for item in label_list[::-1]:
+                    if item["background"] != "":
+                        id = label_list.index(item)
+                        label_list[id + 1].configure(background="red")
+                        break
+                if label_list[0]["background"] == "":
+                    label_list[0].configure(background="red")
+                wrong_count += 1
+            try:
+                entry.delete(0, END)
+            except TclError:
+                print(correct_words, wrong_count)
+
+        def check():
+            global time_value
+            wpm_value = correct_count * 2
+            cpm_value = character_count * 2
+            wpm_label.configure(text=f"WPM:{wpm_value}")
+            cpm_label.configure(text=f"CPM:{cpm_value}")
+
+        time_label = Label(window, text="00:00", font=("Arial", 15))
+        time_label.place(x=400,y=100)
+
+        def counter():
+            global  time_value,counter_id
+            entry = Entry(window, width=45, textvariable=user_var, font=("Arial", 8))
+            entry.place(x=100, y=350)
+            if time_value > 0:
+                time_label.configure(text=f"00:{time_value}")
+                time_value -= 1
+                counter_id = window.after(1000, counter)
+
+            if time_value < 10 and time_value != 0:
+                time_value -= 1
+                time_label.configure(text=f"00:0{time_value}")
+            if time_value == 0:
+                time_label.configure(text=f"00:00")
+
+                entry.destroy()
+                check()
+            window.bind('<space>', speed_test)
+
+
+        def reset():
+            global time_value
+            time_value = 30
+            window.after_cancel(counter_id)
+            for label in label_list:
+                label.configure(background="")
+            wpm_label.configure(text="WPM:", font=("Arial", 15))
+            time_label.configure(text="00:00")
+            cpm_label.configure(text="CPM:", font=("Arial", 15))
+
+        start_button = Button(window, text="Start", command=counter)
+        start_button.place(x=220,y=380)
+
+        reset_button = Button(window, text="reset", command=reset)
+        reset_button.place(x=220,y=400)
+
+        wpm_label = Label(window, text="WPM:", font=("Arial", 12))
+        cpm_label = Label(window, text="CPM:", font=("Arial", 12))
+
+
+        wpm_label.place(x=400,y=125)
+        cpm_label.place(x=400,y=150)
 
 app.mainloop()
